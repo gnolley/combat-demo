970a977720e714691f4f24edd26522b9aa54237e
Tom Knowles <thomasa.knowles@gmail.com>
2025-05-30 18:36:25 +1000

    chore: hooked up abilities to play anim montage

---
+++ Source/CombatSystem/Public/Gameplay/SequenceAttackItem.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "SequenceAttackItem.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSequenceAttackItem
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Animation")
+	TObjectPtr<UAnimMontage> Montage {};
+};
